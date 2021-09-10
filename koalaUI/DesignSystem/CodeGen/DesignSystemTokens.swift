4616813adb6ff6f284d63b4ebbf7fda9c59ff4e5
Leonardo Maffei <leobmaffei@gmail.com>
2021-09-10 08:57:55 -0300

    assets

--- koalaUI/DesignSystem/CodeGen/DesignSystemTokens.swift
+++ koalaUI/DesignSystem/CodeGen/DesignSystemTokens.swift
@@ -1,1 +1,58 @@
+//this file was generated
 
+struct DesignSystem {
+   struct color {
+      struct neutral {
+         let dark = "#000000"
+         let light = "#000000"
+         let darkest = "#000000"
+         let lightest = "#000000"
+      }
+      struct extended {
+      }
+      struct primary {
+         let main = "#000000"
+         let interactive = "#ffffff"
+      }
+      struct secondary {
+         let foreground = "#000000"
+         let success = "#000000"
+         let warning = "#000000"
+         let background = "#000000"
+         let error = "#000000"
+         let hightlight = "#000000"
+      }
+   }
+   struct sizing {
+      struct cell {
+      }
+      struct header {
+      }
+      struct card {
+      }
+      struct icon {
+      }
+   }
+   struct font {
+      struct brand {
+         struct body {
+         }
+         struct head {
+         }
+      }
+      struct product {
+         struct body {
+         }
+         struct head {
+         }
+      }
+   }
+   struct spacing {
+      let tight = 20
+      let relaxed = 100
+      let confortable = 80
+      let cozy = 60
+      let comopact = 40
+   }
+
+}
